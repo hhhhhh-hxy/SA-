3fadfd067f7af6a79d835a6fc514b78468cfec08
zyf <1299519638@qq.com>
2024-06-01 19:39:21 +0800

    pigaizuoye初步创建页面

--- SA-Frontend/src/router/index.js
+++ SA-Frontend/src/router/index.js
@@ -4,6 +4,8 @@ import TeacherCoursework from '../pages/teacherCoursework/teacherCoursework.vue'
 import StuAllHomework from '../pages/stuALLHomework/StuAllHomework.vue'
 import teacher_home from '../pages/teacher_home/jiaoshizhuye.vue'
 
+import Pigaizuoye1 from '../pages/pigaizuoye1/pigaizuoye1.vue';
+import Pigaizuoye2 from '../pages/pigaizuoye2/pigaizuoye2.vue';
 const routes = [
   {
     path: '/login',
@@ -19,6 +21,16 @@ const routes = [
     path: '/stuAllHomework',
     name: 'stuAllHomework',
     component: StuAllHomework,
+  },
+  {
+    path: '/pigaizuoye1',
+    name: 'pigaizuoye1',
+    component: Pigaizuoye1,
+  },
+  {
+    path: '/pigaizuoye2',
+    name: 'pigaizuoye2',
+    component: Pigaizuoye2,
   },
   {
     path: '/teacher_home',
